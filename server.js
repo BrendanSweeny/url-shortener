70fee0a2f9154c157fa15ecc9cfc03e972a822c3
Brendan Sweeny <brendan.sweeny@gmail.com>
2016-04-13 03:57:11 +0000

    Add middleware for styles

--- server.js
+++ server.js
@@ -11,13 +11,17 @@ mongo.connect(process.env.Mongo_URI || localMongoUri, function(err, db) {
   if (err) {
     throw err;
   } else {
-    console.log("Successfully connected to MongoDB...")
+    console.log("Successfully connected to MongoDB...");
+    
+    //App.use() loads the middleware function, express.static(),
+    //which binds the public directory to '/public' for use in index.html
+    app.use("/public", express.static(process.cwd() + "/public"));
     
     routes(app, db);
     
     app.listen(process.env.PORT || 8080, function() {
       console.log("Server listening on PORT 8080...");
-    })
+    });
     
   }
 })
